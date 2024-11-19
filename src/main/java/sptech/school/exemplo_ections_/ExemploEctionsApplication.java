5d896f1dd9d3326b5dfb76ff19d310269604e187
DiegoHessel <diego.verdi@sptech.school>
2024-11-18 21:30:18 -0300

    first commit

---
+++ src/main/java/sptech/school/exemplo_ections_/ExemploEctionsApplication.java
@@ -0,0 +1,13 @@
+package sptech.school.exemplo_ections_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploEctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploEctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sptech/school/exemplo_ections_/ExemploEctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.school.exemplo_ections_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploEctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
